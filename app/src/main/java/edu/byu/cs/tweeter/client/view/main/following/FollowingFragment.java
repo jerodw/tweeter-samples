405d02cf1ea3fa64366aa0a49622ae67d45e08e7
rodham <ken.rodham@gmail.com>
2021-03-18 10:36:10 -0600

    Reorganized some code

--- app/src/main/java/edu/byu/cs/tweeter/client/view/main/following/FollowingFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/view/main/following/FollowingFragment.java
@@ -280,16 +280,12 @@ public class FollowingFragment extends Fragment implements FollowingPresenter.Vi
          * data.
          */
         void loadMoreItems() {
-            // Run this code later on the UI thread
-            final Handler handler = new Handler(Looper.getMainLooper());
-            handler.postDelayed(() -> {
-                isLoading = true;
-                addLoadingFooter();
+            isLoading = true;
+            addLoadingFooter();
 
-                FollowingRequest request = new FollowingRequest(user.getAlias(), PAGE_SIZE, (lastFollowee == null ? null : lastFollowee.getAlias()));
-                presenter.getFollowing(request);
-            }, 0);
-        }
+            FollowingRequest request = new FollowingRequest(user.getAlias(), PAGE_SIZE, (lastFollowee == null ? null : lastFollowee.getAlias()));
+            presenter.getFollowing(request);
+         }
 
         /**
          * Loads the new followees and removes the loading footer.
@@ -372,7 +368,11 @@ public class FollowingFragment extends Fragment implements FollowingPresenter.Vi
             if (!followingRecyclerViewAdapter.isLoading && followingRecyclerViewAdapter.hasMorePages) {
                 if ((visibleItemCount + firstVisibleItemPosition) >=
                         totalItemCount && firstVisibleItemPosition >= 0) {
-                    followingRecyclerViewAdapter.loadMoreItems();
+                    // Run this code later on the UI thread
+                    final Handler handler = new Handler(Looper.getMainLooper());
+                    handler.postDelayed(() -> {
+                        followingRecyclerViewAdapter.loadMoreItems();
+                    }, 0);
                 }
             }
         }
